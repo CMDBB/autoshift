f609fcefd95917b589d34d438b22599d0cdf4ed4
AdrianUlrich <37381718+AdrianUlrich@users.noreply.github.com>
2026-06-25 07:34:43 +0000

    add branch to shift location

---
+++ autoshift/autoshift/doctype/discipline_designation_branch_config_shift_type/discipline_designation_branch_config_shift_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, CMDBB and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class DisciplineDesignationBranchConfigShiftType(Document):
+	pass
