44fa21e62abda542cdbca3d866184849a20ee3eb
AdrianUlrich <37381718+AdrianUlrich@users.noreply.github.com>
2026-07-06 06:39:35 +0000

    ui upgrades

--- autoshift/autoshift/doctype/optimizer_run/optimizer_run_list.js
+++ autoshift/autoshift/doctype/optimizer_run/optimizer_run_list.js
@@ -4,5 +4,10 @@
 frappe.listview_settings["Optimizer Run"] = {
 	// Automatic runs (created by future automated tools, not by a user) are
 	// noise in the default view; they're still reachable by clearing the filter.
-	filters: [["type", "!=", "Automatic"]],
+	onload: function (listview) {
+		frappe.route_options = {
+			type: ["not in", ["Automatic", "Test"]],
+			status: ["not in", ["Approved"]],
+		};
+	},
 };
